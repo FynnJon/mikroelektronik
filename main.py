2c0241b8221aed99b786b38432a32d3a73fdd7b9
FynnJon <fynn.kunzmann@gmx.de>
2023-12-06 12:23:50 +0100

    sfdsf

--- main.py
+++ main.py
@@ -12,16 +12,24 @@ w, mag1, phase1 = signal.bode(tp1, w)
 #TP 4. Ordnung
 #Dazu wurden Nenner und Zähler ausmultipliziert
 tp4 = signal.TransferFunction([33.9585], [1, 9.656, 34.9644, 56.2693, 33.9585])
-#mit 0.414
-#tp4 = signal.TransferFunction([0.029], [1, 1.656, 1.028, 0.284, 0.029])
 w, mag4, phase4 = signal.bode(tp4, w)
+#Butterworth Filter
+tpb = signal.TransferFunction([(1.41)**4], [1, 1.85, 1])
+w, magb, phaseb = signal.bode(tpb, w)
+b, a = signal.butter(4, 1, 'low', analog=True)
+w, h = signal.freqs(b, a, worN=w)
+print(b)
+print(a)
+
 
 fig, axs = plt.subplots(figsize=(8, 5))
 axs.semilogx(w, mag1, color='grey', label='TP 1. Ordnung')
 axs.semilogx(w, mag4, color='blue', label='Reihenschaltung 4x TP 1. Ordnung')
+axs.semilogx(w, h, color='red', label='Butterworth-Filter 4. Ordnung')
 ax = axs.twinx()
 ax.semilogx(w, phase1, color='grey', linestyle='dashed')
 ax.semilogx(w, phase4, color='blue', linestyle='dashed')
+#ax.semilogx(w, phaseb, color='red', linestyle='dashed')
 
 axs.grid()
 axs.set_title('Bodediagramm Tiefpass 1. Ordnung und vierfache Reihenschaltung')
