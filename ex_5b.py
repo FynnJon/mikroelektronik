0a611d13c5856a280a8f140bd0107be86ed86cac
FynnJon <fynn.kunzmann@gmx.de>
2024-01-29 17:13:56 +0100

    4trueteshfd

--- ex_5b.py
+++ ex_5b.py
@@ -11,14 +11,27 @@ def integer_read_array_4d(fname, p_input_shape):
     p_integer_array_4d = np.zeros(p_input_shape)
     with open(fname + '.txt', 'r') as file:
         reader = list(csv.reader(file, delimiter=' '))
-        print(reader)
-        i = 0
+        ix = 0
         for i1 in range(p_input_shape[0]):
             for i2 in range(p_input_shape[1]):
                 for i3 in range(p_input_shape[2]):
                     #for i4 in range(p_input_shape[3]):
-                        p_integer_array_4d[i1, i2, i3, :] = reader[i]
-                        i = i + 1
+                        p_integer_array_4d[i1, i2, i3, :] = reader[ix]
+                        ix = ix + 1
+    return p_integer_array_4d
+
+
+def integer_read_array_4db(fname, p_input_shape):
+    p_integer_array_4d = np.zeros(p_input_shape)
+    with open(fname + '.txt', 'r') as file:
+        reader = list(csv.reader(file, delimiter=' '))
+        ix = 0
+        for i1 in range(p_input_shape[0]):
+            for i2 in range(p_input_shape[1]):
+                for i3 in range(p_input_shape[2]):
+                    #for i4 in range(p_input_shape[3]):
+                        p_integer_array_4d[i1, i2, i3, :] = reader[ix]
+                        ix = ix + 1
     return p_integer_array_4d
 
 
@@ -28,13 +41,13 @@ w = w[:, :, 0, 0]
 i = i[0, :, :, 0]
 w = np.flip(w)
 c = signal.convolve2d(i, w, mode='valid')
-plt.imshow(c, cmap=plt.get_cmap('gray'))
-plt.show()
-print(c)
+#plt.imshow(c, cmap=plt.get_cmap('gray'))
+#plt.show()
+#print(c)
 
 
-a = integer_read_array_4d("output", (1, 24, 24, 1))
-plt.imshow(a[0, :, :, 0], cmap=plt.get_cmap('gray'))
+a = integer_read_array_4db("output", (1, 24, 24, 2))
+plt.imshow(a[0, :, :, 1], cmap=plt.get_cmap('gray'))
 plt.show()
-print(a)
+#print(a)
 
